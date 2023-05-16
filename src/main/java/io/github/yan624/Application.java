159009321ff4fe23f1bae67edab85f33b7f5ecf9
yorz <vccorz@foxmail.com>
2023-05-16 23:15:33 +0800

    初始化springboot应用

---
+++ src/main/java/io/github/yan624/Application.java
@@ -0,0 +1,16 @@
+package io.github.yan624;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yorz
+ * @since 2023-05-16
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
